09fc3999958dd51f850bd1d8c8b3de44f7f0997a
raihanraju007 <raihanraju007@gmail.com>
2025-02-04 13:15:41 +0600

    CRUD Operations complate Without database

--- src/main/java/com/raju/firstjobapp/job/JobController.java
+++ src/main/java/com/raju/firstjobapp/job/JobController.java
@@ -1,14 +1,14 @@
 package com.raju.firstjobapp.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+//@RequestMapping("/api")
 public class JobController {
 
     private JobService jobService;
@@ -18,12 +18,38 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+        return ResponseEntity.ok( jobService.findAll());
     }
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added successfully",HttpStatus.CREATED);
+    }
+
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
+        Job job = jobService.getJobById(id);
+        if (job != null)
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean deleted = jobService.deleteJobById(id);
+        if (deleted)
+            return new ResponseEntity<>("Job deleted successfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+//    @RequestMapping(value = "/jobs/{id}", method = RequestMethod.PUT)
+    @PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id,
+                                            @RequestBody Job updateJob){
+        boolean updated = jobService.updateJob(id,updateJob);
+        if (updated)
+            return new  ResponseEntity<>("Job update successfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/raju/firstjobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/raju/firstjobapp/job/impl/JobServiceImpl.java
@@ -5,6 +5,7 @@ import com.raju.firstjobapp.job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -21,5 +22,43 @@ public class JobServiceImpl implements JobService {
     public void createJob(Job job) {
         job.setId(nextId++);
         jobs.add(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for (Job job : jobs){
+            if (job.getId().equals(id)){
+                return job;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()){
+            Job job = iterator.next();
+            if (job.getId().equals(id)){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updateJob) {
+        for (Job job : jobs){
+            if (job.getId().equals(id)){
+                job.setTitle(updateJob.getTitle());
+                job.setDescription(updateJob.getDescription());
+                job.setMinSalary(updateJob.getMinSalary());
+                job.setMaxSalary(updateJob.getMaxSalary());
+                job.setLocation(updateJob.getLocation());
+                return  true;
+            }
+        }
+        return false;
     }
 }
